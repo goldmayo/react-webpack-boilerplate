64e15da3ff5aceaa5b8453b30de9281a9df7b4d9
HyunSJ <dudtod1596@gmail.com>
2021-08-18 01:33:51 +0900

    remove options of babel-loader in webpack.common.js, it makes undefined error in deploy.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -26,9 +26,6 @@ module.exports = {
         use: [
           {
             loader: "babel-loader",
-            options: {
-              plugins: ["react-refresh/babel"],
-            },
           },
         ],
       },
